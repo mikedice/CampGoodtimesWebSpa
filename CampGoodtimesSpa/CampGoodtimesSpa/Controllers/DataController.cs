161319b9622cd2f71e3faea6ed4c00852b6fb1ca
mikedice <mikedice417@hotmail.com>
2014-03-23 21:06:45 -0700

    spa version of goodtimes

---
+++ CampGoodtimesSpa/CampGoodtimesSpa/Controllers/DataController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CampGoodtimesSpa.Controllers
+{
+    public class DataController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<object> NewsItems()
+        {
+            return new object[] { 
+                new {name="object 1", city="Redmond"},
+                new {name="object 2", city="Seattle"}
+            };
+        }
+
+        //// GET api/api/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/api
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/api/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/api/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ CampGoodtimesSpa/CampGoodtimesSpa/Scripts/app.js
@@ -0,0 +1,42 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="angular.js" />
+/// <reference path="angular-route.js" />
+
+var app = angular.module('GoodtimesApp', ['ngRoute', 'ngAnimate']);
+
+app.config(function ($routeProvider) {
+    $routeProvider.when('/', { controller: 'HomeViewController', templateUrl: '/Views/HomePartial.html' })
+                 .otherwise({ redirectTo: '/' });
+});
+
+
+// Factory object for producing data
+app.factory('newsItemFactory', function ($http) {
+
+    var factory = {};
+    factory.getNewItems = function () { return $http.get("/api/Data/NewsItems"); }
+    return factory;
+});
+
+
+app.controller('HomeViewController', function ($scope, newsItemFactory) {
+    init();
+
+    function init() {
+        newsItemFactory.getNewItems().success(function (data) {
+            $scope.newsItems = data;
+        });
+    };
+
+    $scope.becomeAVolunteerClicked = function () {
+        window.alert("volunteer!");
+    };
+
+    $scope.becomeACamperClicked = function () {
+        window.alert("camper!");
+    };
+
+    $scope.donateClicked = function () {
+        window.alert("donate!");
+    };
+});
