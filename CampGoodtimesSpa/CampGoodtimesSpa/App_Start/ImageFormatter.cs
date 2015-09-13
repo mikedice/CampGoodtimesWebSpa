97d0e9dd8d647fc682630c07a26eb726b60b9e5c
Mike Dice <mikedice417@hotmail.com>
2015-09-12 17:14:44 -0700

    updating to vs2015

--- CampGoodtimesSpa/CampGoodtimesSpa/App_Start/ImageFormatter.cs
+++ CampGoodtimesSpa/CampGoodtimesSpa/App_Start/ImageFormatter.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
-using System.Web;
+using System.Net.Http.Formatting;
 using System.Threading.Tasks;
-using System.Threading;
 using System.IO;
 
 namespace CampGoodtimesSpa.Models
